63f7d68591e11d34051fdd99d002bdd2a97f4017
EmBP19 <emmanuelbpal@gmail.com>
2021-03-14 13:41:10 -0600

    Agrega lab 06 - Tienda en linea

--- JS/Lab06.js
+++ JS/Lab06.js
@@ -1,3 +1,4 @@
+//Validar password 
 var password = document.getElementById("password")
   , confirm_password = document.getElementById("confirm_password");
 
@@ -7,9 +8,30 @@ function validarPassword(){
   if(password.value != confirm_password.value) {
     confirm_password.setCustomValidity("La password no coincide");
   } else {
-    confirm_password.setCustomValidity('');
+    confirm_password.setCustomValidity('La password coincide =D');
   }
 }
 
 password.onchange = validarPassword;
 confirm_password.onkeyup = validarPassword;
+
+//Tienda 
+const numpadrino = document.getElementById("godfather");
+const numlalaland = document.getElementById("lalaland");
+const numstarwars = document.getElementById("starwars");
+const iva;
+const total;
+const formato = document.getElementById("form");
+
+formato.onsubmit = addtotal(event){
+    event.preventDefault();
+   var t = (numpadrino.value*15)+(numlalaland.value*20)+(numstarwars.value*25);
+   total.inner
+   // alert("El precio total es de: " + total );
+   
+    document.getElementById("total").innerHTML = "El precio total es de: " + t ;
+   // T.innerHTML = "El precio total es de: " + total;
+    
+}
+
+
